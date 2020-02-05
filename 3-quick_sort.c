bb6e523e3e10636902bafe218a951dc7733775a2
Cybernuki <1164@holbertonschool.com>
2020-02-05 12:47:04 -0500

    Challenge 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,52 @@
 #include "sort.h"
 
 /**
+ *  partition - is the partition of the quicksort
+ *  @array: is the array
+ *  @low: is the low index
+ *  @high: is the high index
+ *  Return: the pivot's index at the end
+ */
+int partition(int *array, int low, int high)
+{
+	int i = low, j, tmp;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < array[high])
+		{
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			i++;
+		}
+	}
+	tmp = array[high];
+	array[high] = array[i];
+	array[i] = tmp;
+	return (i);
+}
+
+/**
+ *  sort - is the quick sort
+ *  @array: is the array
+ *  @low: is the low index
+ *  @high: is the high index
+ *  Return: nothing
+ */
+void sort(int *array, int low, int high)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high);
+		sort(array, low, p - 1);
+		sort(array, p + 1, high);
+	}
+}
+
+/**
  *  quick_sort - sorts an array using quick sort
  *  each swamp prints the array
  *  @array: is the given array
@@ -9,5 +55,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    
+	if (!array)
+		return;
+	sort(array, 0, size - 1);
 }
