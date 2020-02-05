e88c5aa3cf2343e67de69da0dfd8f671890e8cd3
michelalejo <michelmolina65@gmail.com>
2020-02-04 22:24:00 -0500

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,5 +9,22 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, tmp, swap;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[i] && array[j] < array[swap])
+				swap = j;
+		}
+		if (swap != i)
+		{
+			tmp = array[i];
+			array[i] = array[swap];
+			array[swap] = store;
+			print_array(array, size);
+		}
+	}
 }
